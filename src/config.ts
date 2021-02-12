2c699a7d61021d3bb8fb6d702568156e8ace83dc
Greg Taschuk <gregorytaschuk@gmail.com>
2021-02-12 10:35:59 -0800

    set correct heroku deploy environment

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,5 @@
 // GCP Project ID - should be common across the cloud function and the Bigquery Instance
-export const projectId = 'balancer-rewards-api';
-
-// Bigquery Dataset Name
-export const dataset = 'balancer_rewards';
-
-// Bigquery Table names - shouold be within the `dataset` above
-export const poolsTableName = 'pools';
-export const liquidityProviderTableName = 'wallets';
+export const projectId = process.env.GCLOUD_PROJECT || 'balancer-rewards-api';
+export const dataset = process.env.BIGQUERY_DATASET || 'bal_mining_estimates';
+export const poolsTableName = process.env.BIGQUERY_POOL_TABLE ||  'pool_estimates';
+export const liquidityProviderTableName = process.env.BIGQUERY_LP_TABLE || 'lp_estimates';
